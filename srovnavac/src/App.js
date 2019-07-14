7f1c3019d64aed4cc82f69538db009a0dae31778
Honzaik <honzaik@seznam.cz>
2019-07-14 23:55:55 +0200

    kostra

---
+++ srovnavac/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Container, Header } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Header size="huge" className="frontHeader" >Srovnávač potravin</Header>
+        <h1>ayyyy</h1>
+        <p>whats up</p>
+        <button class="ui button">Click Herevv</button>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
